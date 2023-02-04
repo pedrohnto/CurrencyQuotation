d30da81eb2f6026b7ef44fa48aa067697118d9a3
Pedro Neto <pedropicui@gmail.com>
2023-02-04 14:16:25 -0300

    Add endpoints to Last and Daily currency quotation

--- CurrencyAPI/Controllers/CurrencyQuotationController.cs
+++ CurrencyAPI/Controllers/CurrencyQuotationController.cs
@@ -10,19 +10,34 @@ namespace CurrencyAPI.Controllers
     public class CurrencyQuotationController : ControllerBase
     {
 
+        private const string API_CURRENCY = "https://economia.awesomeapi.com.br/json";
+
         public CurrencyQuotationController()
         {
         }
 
-        [HttpGet]
-        public async Task<string> getCurrencyQuotationAsync()
+        [HttpGet("last")]
+        public async Task<string> getLastCurrencyQuotation([FromQuery] string currency)
+        {
+            return await getLastCurrencyQuotationAsync(currency);
+        }
+
+        [HttpGet("daily")]
+        public async Task<string> getDailyCurrencyQuotation([FromQuery] string currency)
         {
             using var client = new HttpClient();
-            var response = await client.GetAsync("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL");
+            var response = await client.GetAsync($"{API_CURRENCY}/daily/{currency}-BRL/10");
             var content = await response.Content.ReadAsStringAsync();
             return content;
         }
 
+        private async Task<string> getLastCurrencyQuotationAsync(string currency)
+        {
+            using var client = new HttpClient();
+            var response = await client.GetAsync($"{API_BTC}{currency}");
+            var content = await response.Content.ReadAsStringAsync();
+            return content;
+        }
 
       
     }
